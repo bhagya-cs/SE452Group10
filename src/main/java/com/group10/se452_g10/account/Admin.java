42fa06b03e01d560876438d2d8dd2b3e1c12b288
Jared Wang <wy.jared@gmail.com>
2023-04-14 22:58:00 -0500

    Revert "Add Some data entities"
    This reverts commit 03a4dfb990ee7144970adaaddb24ca1887c347f1.

--- src/main/java/com/group10/se452_g10/account/Admin.java
+++
@@ -1,7 +0,0 @@
-package com.group10.se452_g10.account;
-
-import jakarta.persistence.Entity;
-
-@Entity(name = "admins")
-public class Admin extends Account {
-}
--- src/main/java/com/group10/se452_g10/account/Student.java
+++
@@ -1,7 +0,0 @@
-package com.group10.se452_g10.account;
-
-import jakarta.persistence.Entity;
-
-@Entity(name = "students")
-public class Student extends User {
-}
