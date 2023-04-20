cfce5b58cfdea67fa0a6f9d412ae91baf122076f
SrinivasDasari22 <dsaikrishna333@gmail.com>
2023-04-19 22:40:19 -0500

    .

--- src/main/java/com/group10/se452_g10/payment/PaymentMethodRepository.java
+++ src/main/java/com/group10/se452_g10/payment/PaymentMethodRepository.java
@@ -1,8 +1,32 @@
 package com.group10.se452_g10.payment;
 
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PaymentMethodRepository extends JpaRepository<PaymentMethod, Long> {
+
+    @Override
+    List<PaymentMethod> findAll(Sort sort);
+
+    @Override
+    List<PaymentMethod> findAllById(Iterable<Long> longs);
+
+    @Override
+    void deleteAll();
+
+    @Override
+    void delete(PaymentMethod entity);
+
+    @Override
+    void deleteAllById(Iterable<? extends Long> longs);
+
+    @Override
+    long count();
+
+    @Override
+    boolean existsById(Long aLong);
 
 
 }
