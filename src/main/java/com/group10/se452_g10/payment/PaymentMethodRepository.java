5e004934d55ed35cb85f40566c031596e5eb39d9
SrinivasDasari22 <dsaikrishna333@gmail.com>
2023-04-19 22:58:46 -0500

    .

--- src/main/java/com/group10/se452_g10/payment/PaymentMethodRepository.java
+++ src/main/java/com/group10/se452_g10/payment/PaymentMethodRepository.java
@@ -29,4 +29,6 @@ public interface PaymentMethodRepository extends JpaRepository<PaymentMethod, Lo
     boolean existsById(Long aLong);
 
 
+
+
 }
---
+++ src/main/java/com/group10/se452_g10/payment/PaymentMethodService.java
@@ -0,0 +1,21 @@
+//package com.group10.se452_g10.payment;
+//
+//
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//public class PaymentMethodService {
+//
+//    private final PaymentMethodRepository paymentMethodRepository;
+//
+//    public PaymentMethodService(PaymentMethodRepository paymentMethodRepository) {
+//        this.paymentMethodRepository = paymentMethodRepository;
+//    }
+//
+//    public PaymentMethod createPayment(PaymentMethod paymentMethod){
+//
+//        PaymentMethod paymentMethod1 = new PaymentMethod(paymentMethod.getId()){
+//
+//        }
+//    }
+//}
