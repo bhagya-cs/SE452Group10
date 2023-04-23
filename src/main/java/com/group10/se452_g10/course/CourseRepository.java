274583d30198906a17798ed09f970701162c7f39
Jared Wang <wy.jared@gmail.com>
2023-04-22 22:32:04 -0500

    Add search method in Course Repo/Service,

--- src/main/java/com/group10/se452_g10/course/CourseRepository.java
+++ src/main/java/com/group10/se452_g10/course/CourseRepository.java
@@ -1,7 +1,16 @@
 package com.group10.se452_g10.course;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface CourseRepository extends JpaRepository<Course, Long> {
-
+    @Query("SELECT c FROM courses c " +
+            "WHERE c.dept ILIKE %:keyword% " +
+            "OR c.num ILIKE %:keyword% " +
+            "OR c.name ILIKE %:keyword% " +
+            "OR CONCAT(c.dept, c.num) ILIKE %:keyword%")
+    List<Course> search(@Param("keyword") String keyword);
 }
