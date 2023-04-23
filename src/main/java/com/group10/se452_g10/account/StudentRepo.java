1ea6c9ec7473e9d311b257a9a93ab9201bc8b338
Ayyub Jose <ajose3@depaul.edu>
2023-04-23 14:14:03 -0500

    Updating repo from String to Long

--- src/main/java/com/group10/se452_g10/account/StudentRepo.java
+++ src/main/java/com/group10/se452_g10/account/StudentRepo.java
@@ -2,7 +2,7 @@ package com.group10.se452_g10.account;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface StudentRepo extends JpaRepository<Student, String> {
+public interface StudentRepo extends JpaRepository<Student, Long> {
 
 
 }
