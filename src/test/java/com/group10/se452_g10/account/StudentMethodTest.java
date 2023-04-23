532ed91eaa6f4b243f765d82d42a6be22bcae02a
Ayyub Jose <ajose3@depaul.edu>
2023-04-23 15:16:55 -0500

    Update of Student , fixing an error

--- src/test/java/com/group10/se452_g10/account/StudentMethodTest.java
+++ src/test/java/com/group10/se452_g10/account/StudentMethodTest.java
@@ -1,19 +1,10 @@
 package com.group10.se452_g10.account;
 
 
-import com.group10.se452_g10.payment.PaymentRecord;
-import org.aspectj.lang.annotation.Before;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.jdbc.Sql;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.time.LocalDate;
-import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
