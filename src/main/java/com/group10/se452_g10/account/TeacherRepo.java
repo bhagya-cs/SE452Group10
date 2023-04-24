8c115fc037358bc563a23f689f820fa69aac12ca
Ayyub Jose <ajose3@depaul.edu>
2023-04-24 17:13:18 -0500

    Adding fucntions to repo interface

--- src/main/java/com/group10/se452_g10/account/TeacherRepo.java
+++ src/main/java/com/group10/se452_g10/account/TeacherRepo.java
@@ -1,6 +1,15 @@
 package com.group10.se452_g10.account;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface TeacherRepo extends JpaRepository<Teacher, Long> {
+
+
+
+
+
 }
