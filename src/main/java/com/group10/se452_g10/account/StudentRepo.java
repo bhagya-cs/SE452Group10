e0e343247130881aeba6c9df65723fa65221bbe8
Ayyub Jose <ajose3@depaul.edu>
2023-04-24 17:13:18 -0500

    Adding fucntions to repo interface

--- src/main/java/com/group10/se452_g10/account/StudentRepo.java
+++ src/main/java/com/group10/se452_g10/account/StudentRepo.java
@@ -1,33 +1,17 @@
 package com.group10.se452_g10.account;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface StudentRepo extends JpaRepository<Student, Long> {
 
 //
+    public List<Student> findByAgeLessThanEqual(long age);
 
-
-//    @Query("SELECT c FROM students c WHERE c.lastName ILIKE %:keyword%" )
-//    public List<Student> findByLastName(@Param("name")String name);
-
-    Optional<User> findByUsername(String username);
-
-    Boolean existsByUsername(String username);
-
-    Boolean existsByEmail(String email);
-
-    @Query("select firstName , lastName , age from teachers where age<= :age")
-    public List<Student> findByAgeLessThanEqual(@Param("age")long age);
-
-    //@Query("SELECT * from students where firstName ILIKE %:name%")
-    public List<Student> findByFirstName(@Param("name") String name);
+    public List<Student> findByFirstName(String name);
 
 
 }
--- src/main/java/com/group10/se452_g10/account/TeacherRepo.java
+++ src/main/java/com/group10/se452_g10/account/TeacherRepo.java
@@ -1,10 +1,6 @@
 package com.group10.se452_g10.account;
 
-import com.group10.se452_g10.course.Course;
-import com.group10.se452_g10.course.Quarter;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,16 +9,6 @@ import java.util.List;
 public interface TeacherRepo extends JpaRepository<Teacher, Long> {
 
 
-
-    @Query("select firstName , lastName , age from teachers where age<= :age")
-    public List<Teacher> findByAgeLessThanEqual(@Param("age") long age);
-
-    @Query("SELECT c FROM teachers c WHERE c.firstName ILIKE %:keyword%" )
-    public List<Teacher> findByFirstName(@Param("name")String name);
-
-
-    @Query("SELECT c FROM teachers c WHERE c.lastName ILIKE %:keyword%" )
-    public List<Teacher> findByLastName(@Param("name")String name);
 
 
 
