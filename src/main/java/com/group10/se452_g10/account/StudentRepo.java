e6c86b77d7ab588aacea70a2b3f1f1bf699edfc9
Ayyub Jose <ajose3@depaul.edu>
2023-04-24 17:13:18 -0500

    Adding fucntions to repo interface

--- src/main/java/com/group10/se452_g10/account/StudentRepo.java
+++ src/main/java/com/group10/se452_g10/account/StudentRepo.java
@@ -1,8 +1,17 @@
 package com.group10.se452_g10.account;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface StudentRepo extends JpaRepository<Student, Long> {
+
+//
+    public List<Student> findByAgeLessThanEqual(long age);
+
+    public List<Student> findByFirstName(String name);
 
 
 }
