0010daeaa4884143af5ede74342ea05ed5ac392b
Ayyub Jose <ajose3@depaul.edu>
2023-04-24 17:13:02 -0500

    Update of Student test, adding new test functions

--- src/test/java/com/group10/se452_g10/account/TeacherMethodTest.java
+++ src/test/java/com/group10/se452_g10/account/TeacherMethodTest.java
@@ -6,13 +6,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @DataJpaTest
 @ActiveProfiles("test")
-//@Sql({"/data-teacher-test.sql"})
 public class TeacherMethodTest {
 
     @Autowired
