031ae14b6b150aa4b02ed9661cfe565e42d18094
Ayyub Jose <ajose3@depaul.edu>
2023-04-24 17:13:02 -0500

    Update of Student test, adding new test functions

--- src/test/java/com/group10/se452_g10/account/StudentMethodTest.java
+++ src/test/java/com/group10/se452_g10/account/StudentMethodTest.java
@@ -4,25 +4,62 @@ package com.group10.se452_g10.account;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @DataJpaTest
-@Transactional
+@ActiveProfiles("test")
+//Sql({"/data-student-test.sql"})
 public class StudentMethodTest {
 
     @Autowired
     private StudentRepo studentRepo;
 
 
+
+    @Test
+    public void testFindByAgeLessThanEqual() {
+        Student s_1 = new Student();
+        s_1.setLastName("Chicago");
+        s_1.setAge(20L);
+        studentRepo.save(s_1);
+
+        Student s_2 = new Student();
+        s_2.setLastName("France");
+        s_1.setAge(20L);
+        studentRepo.save(s_2);
+
+
+        Student s_3 = new Student();
+        s_1.setLastName("Chicago");
+        s_1.setAge(10L);
+        studentRepo.save(s_3);
+
+
+        Student s_4 = new Student();
+        s_2.setLastName("France");
+        s_1.setAge(30L);
+        studentRepo.save(s_4);
+
+        long count = studentRepo.count();
+
+        List<Student> lessThanDrinking = studentRepo.findByAgeLessThanEqual(241L);
+
+        assertEquals(4, count);
+        assertEquals(4, lessThanDrinking.size());
+
+    }
     @Test
     public void testCreationStudent() {
 
         studentRepo.deleteAll();
         long beforeCount = studentRepo.count();
-
 
         Student s_1 = new Student();
         s_1.setAddress("Chicago");
@@ -61,12 +98,13 @@ public class StudentMethodTest {
         s_1.setLastName("Jose");
         s_1.setGender("M");
 
+        studentRepo.save(s_1);
 
-
-        Student s1_test = studentRepo.save(s_1);
+        long count_one = studentRepo.count();
+        assertEquals(1, count_one);
 
     }
-//
+
     @Test
     public void testDeleteStudentRecord() {
 
@@ -102,12 +140,6 @@ public class StudentMethodTest {
 
 
     }
-//
-//    @Test
-//    public void testReadStudentRecord() {
-//
-//
-//
-//    }
+
 }
 
