e83ffdd0cfee4710eba4c2566f73470d934dd367
Ayyub Jose <ajose3@depaul.edu>
2023-04-24 17:13:48 -0500

    Deletign classes and packages that do not belong to me

--- src/main/java/com/group10/se452_g10/payment/PaymentRecordRepository.java
+++
@@ -1,7 +0,0 @@
-package com.group10.se452_g10.payment;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PaymentRecordRepository extends JpaRepository<PaymentRecord,Long> {
-
-}
