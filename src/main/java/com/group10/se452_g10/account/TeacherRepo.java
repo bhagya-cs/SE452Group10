8bb2060bb6767ef8b2598d7d1bc921de414791f3
Ayyub Jose <ajose3@depaul.edu>
2023-05-14 21:42:42 -0500

    Adding query to repo

--- src/main/java/com/group10/se452_g10/account/TeacherRepo.java
+++ src/main/java/com/group10/se452_g10/account/TeacherRepo.java
@@ -1,6 +1,8 @@
 package com.group10.se452_g10.account;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,9 +12,11 @@ public interface TeacherRepo extends JpaRepository<Teacher, Long> {
 
 
 
-    public List<Teacher> findByAgeLessThanEqual(long age);
+    @Query("select firstName , lastName , age from teachers where age<= :age")
+    public List<Teacher> findByAgeLessThanEqual(@Param("age") long age);
 
-    public List<Teacher> findByFirstName(String name);
+    @Query("SELECT t from teachers where t.firstName ILIKE %:name%")
+    public List<Teacher> findByFirstName(@Param("name")String name);
 
 
 
