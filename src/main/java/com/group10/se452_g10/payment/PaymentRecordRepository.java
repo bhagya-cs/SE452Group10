61ce9778e48967e82344685a78d512835dbd3fe8
Ayyub Jose <ajose3@depaul.edu>
2023-05-27 19:47:52 -0500

    Revert "Deletign classes and packages that do not belong to me"
    This reverts commit e83ffdd0cfee4710eba4c2566f73470d934dd367.

---
+++ src/main/java/com/group10/se452_g10/payment/PaymentRecordRepository.java
@@ -0,0 +1,7 @@
+package com.group10.se452_g10.payment;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PaymentRecordRepository extends JpaRepository<PaymentRecord,Long> {
+
+}
