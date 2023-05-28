8be9c17ce6eb14eece39e079c2ccfea1b082ff95
Ayyub Jose <ajose3@depaul.edu>
2023-05-27 20:06:20 -0500

    Delete sql files in test

--- src/test/resources/data-student-test.sql
+++
@@ -1,4 +0,0 @@
-insert into students(first_name, age) values('James', 18);
-insert into students(first_name, age) values('Daniel', 22);
-insert into students(first_name, age) values('Mac', 17);
-insert into students(first_name, age) values('Jack', 21);
